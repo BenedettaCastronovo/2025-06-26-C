efc1b63407916a2e365515062ea2cad270b526a9
benedetta <s327261@studenti.polito.it>
2026-06-19 18:00:35 +0200

    prima parte

--- UI/controller.py
+++ UI/controller.py
@@ -7,13 +7,41 @@ class Controller:
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        self._year1 = None
+        self._year2 = None
+
+
+    def fillY(self):
+        self._years = self._model.getY()
+        for y in self._years:
+            self._view._ddYear1.options.append(ft.dropdown.Option(y))
+            self._view._ddYear2.options.append(ft.dropdown.Option(y))
+
+        self._view.update_page()
 
     def handleBuildGraph(self, e):
-        pass
+        self._year1 = self._view._ddYear1.value
+        self._year2 = self._view._ddYear2.value
+        if self._year1 is None or self._year2 is None:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("seleziona un anno"))
+        if self._view._ddYear1.value > self._view._ddYear2.value:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("seleziona gli anni nell'ordine corretto"))
+            return
+
+        self._model.creaGrafo(self._year1, self._year2)
+        nodi = self._model.getNodi()
+        archi = self._model.getArchi()
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text("grafo creato correttamente"))
+        self._view.update_page()
 
 
     def handlePrintDetails(self, e):
-        pass
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text(f"nodi: {nodi}, archi: {archi}"))
+        self._view.update_page()
 
     def handleCercaTeamSfortunati(self, e):
         pass
--- database/DAO.py
+++ database/DAO.py
@@ -18,3 +18,21 @@ class DAO():
         cnx.close()
         return res
 
+    @staticmethod
+    def getY():
+        cnx = DBConnect.get_connection()
+        cursor = cnx.cursor(dictionary=True)
+        anni = []
+        query= """                    
+             select distinct `year` 
+             from seasons 
+             order by `year`
+            """
+        cursor.execute(query)
+        for row in cursor:
+            anni.append(row["year"])
+
+        cursor.close()
+        cnx.close()
+
+        return anni
